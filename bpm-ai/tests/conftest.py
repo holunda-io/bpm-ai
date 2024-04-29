78ffd9460848b1e7e262b41f80fcd490d536c682
Benjoyo <bennet.krause@gmx.de>
2024-04-29 16:41:23 +0200

    add hf cache cleanup fixture

--- bpm-ai/tests/conftest.py
+++ bpm-ai/tests/conftest.py
@@ -1,9 +1,12 @@
 import os
+import platform
+import shutil
 
 import pytest
 from bpm_ai_core.llm.anthropic_chat.anthropic_chat import ChatAnthropic
 from bpm_ai_core.llm.openai_chat.openai_chat import ChatOpenAI
 from bpm_ai_core.util.rpc import remote_object
+from bpm_ai_inference.util.hf import hf_home
 
 
 @pytest.fixture
@@ -17,3 +20,11 @@ def local_llm():
 @pytest.fixture
 def llm():
     return None #remote_object("ChatLlamaCpp", "0.0.0.0", 6666, model="QuantFactory/Phi-3-mini-4k-instruct-GGUF")
+
+
+@pytest.fixture(autouse=True, scope="module")
+def cleanup():
+    yield
+    if platform.system() != "Darwin":
+        cache_dir = hf_home()
+        shutil.rmtree(cache_dir, ignore_errors=True)
